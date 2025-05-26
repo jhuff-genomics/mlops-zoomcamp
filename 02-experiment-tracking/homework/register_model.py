e13be220da8c12c710356574d67434e0da7331d2
Jason Huff <jason.huff@effem.com>
2025-05-26 15:48:49 -0700

    Fix names to ids in search and asc rmse order

--- 02-experiment-tracking/homework/register_model.py
+++ 02-experiment-tracking/homework/register_model.py
@@ -72,10 +72,10 @@ def run_register_model(data_path: str, top_n: int):
     # Select the model with the lowest test RMSE
     experiment = client.get_experiment_by_name(EXPERIMENT_NAME)
     best_run = client.search_runs(
-        experiment_names=[EXPERIMENT_NAME],
+        experiment_ids=[EXPERIMENT_NAME],
         run_view_type=ViewType.ACTIVE_ONLY,
         max_results=1,
-        order_by=["metrics.rmse DESC"],
+        order_by=["metrics.rmse ASC"],
     )[0]
 
     # Register the best model
