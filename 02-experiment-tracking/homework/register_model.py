c6f98f2a7be967237d2193f2481c4e0b408b60ef
Jason Huff <jason.huff@effem.com>
2025-05-26 15:42:02 -0700

    "Search for and register best rmse"

--- 02-experiment-tracking/homework/register_model.py
+++ 02-experiment-tracking/homework/register_model.py
@@ -71,10 +71,15 @@ def run_register_model(data_path: str, top_n: int):
 
     # Select the model with the lowest test RMSE
     experiment = client.get_experiment_by_name(EXPERIMENT_NAME)
-    # best_run = client.search_runs( ...  )[0]
+    best_run = client.search_runs(
+        experiment_names=[EXPERIMENT_NAME],
+        run_view_type=ViewType.ACTIVE_ONLY,
+        max_results=1,
+        order_by=["metrics.rmse DESC"],
+    )[0]
 
     # Register the best model
-    # mlflow.register_model( ... )
+    mlflow.register_model(best_run, "nyc-bestest-rmse")
 
 
 if __name__ == '__main__':
