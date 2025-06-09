6a1632c55675a43368959354f7f7db6cb80e0488
jhuff-genomics <jhuff-genomics>
2025-06-09 02:22:53 +0000

    Switch to linear regression and reduce MLFlow autologging

--- 03-orchestration/code/duration-prediction.py
+++ 03-orchestration/code/duration-prediction.py
@@ -1,12 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-#import pickle
-#from pathlib import Path
-
 import pandas as pd
-import xgboost as xgb
 
+from sklearn.linear_model import LinearRegression
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.metrics import root_mean_squared_error
 
@@ -14,10 +8,6 @@ import mlflow
 
 mlflow.set_tracking_uri("databricks")
 mlflow.set_experiment("/nyc-taxi-experiment")
-
-#models_folder = Path('models')
-#models_folder.mkdir(exist_ok=True)
-
 
 
 def read_dataframe(year, month):
@@ -53,38 +43,13 @@ def create_X(df, dv=None):
 
 def train_model(X_train, y_train, X_val, y_val, dv):
     with mlflow.start_run() as run:
-        train = xgb.DMatrix(X_train, label=y_train)
-        valid = xgb.DMatrix(X_val, label=y_val)
 
-        best_params = {
-            'learning_rate': 0.09585355369315604,
-            'max_depth': 30,
-            'min_child_weight': 1.060597050922164,
-            'objective': 'reg:linear',
-            'reg_alpha': 0.018060244040060163,
-            'reg_lambda': 0.011658731377413597,
-            'seed': 42
-        }
+        lr = LinearRegression()
+        lr.fit(X_train, y_train)
 
-        mlflow.log_params(best_params)
-
-        booster = xgb.train(
-            params=best_params,
-            dtrain=train,
-            num_boost_round=30,
-            evals=[(valid, 'validation')],
-            early_stopping_rounds=50
-        )
-
-        y_pred = booster.predict(valid)
+        y_pred = lr.predict(X_val)
         rmse = root_mean_squared_error(y_val, y_pred)
         mlflow.log_metric("rmse", rmse)
-
-#        with open("preprocessor.b", "wb") as f_out:
-#            pickle.dump(dv, f_out)
-#        mlflow.log_artifact("preprocessor.b", artifact_path="preprocessor")
-
-        mlflow.xgboost.log_model(booster, artifact_path="models_mlflow")
 
         return run.info.run_id
 
@@ -103,6 +68,8 @@ def run(year, month):
     y_train = df_train[target].values
     y_val = df_val[target].values
 
+    mlflow.autolog()
+    mlflow.sklearn.autolog(log_datasets=False, log_model_signatures=False)
     run_id = train_model(X_train, y_train, X_val, y_val, dv)
     print(f"MLflow run_id: {run_id}")
     return run_id
