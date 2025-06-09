c0656d26afd8a92372565e390957ab3bf0a38510
jhuff-genomics <jhuff-genomics>
2025-06-09 01:22:38 +0000

    Adapt to Yellow taxi schema

--- 03-orchestration/code/duration-prediction.py
+++ 03-orchestration/code/duration-prediction.py
@@ -24,7 +24,7 @@ def read_dataframe(year, month):
     url = f'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year}-{month:02d}.parquet'
     df = pd.read_parquet(url)
 
-    df['duration'] = df.lpep_dropoff_datetime - df.lpep_pickup_datetime
+    df['duration'] = df.tpep_dropoff_datetime - df.tpep_pickup_datetime
     df.duration = df.duration.apply(lambda td: td.total_seconds() / 60)
 
     df = df[(df.duration >= 1) & (df.duration <= 60)]
